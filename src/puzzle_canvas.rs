28725ef91c22c528e0fe080d29d2e37eb3847769
Paul Burke <pburke8@gatech.edu>
2021-03-07 13:17:09 -0500

    Add square highlighting on hover.

--- src/puzzle_canvas.rs
+++ src/puzzle_canvas.rs
@@ -1,5 +1,5 @@
 use iced::{
-       canvas::{self, Cursor, Path, Stroke, Text},
+       canvas::{self, event::{self, Event}, Cursor, Path, Stroke, Text},
        Color, Point, Rectangle, Size, HorizontalAlignment, VerticalAlignment,
        };
 use crate::puzzle_backend;
@@ -14,10 +14,13 @@ struct SquareUIInfo {
 pub struct PuzzleCanvas {
     backend: puzzle_backend::Puzzle,
     dim: u32,
+    cursor_pos: Point,
+    highlighted_square: Option<(u32, u32)>,
     grid_cache: canvas::Cache,
     label_cache: canvas::Cache,
     content_cache: canvas::Cache,
     modifier_cache: canvas::Cache,
+    highlighter_cache: canvas::Cache,
 }
 
 impl PuzzleCanvas {
@@ -26,15 +29,35 @@ impl PuzzleCanvas {
         PuzzleCanvas {
             backend: puzzle_backend::Puzzle::new(variant),
             dim: d as u32,
+            cursor_pos: Point::new(0.0,0.0),
+            highlighted_square: None,
             grid_cache: Default::default(),
             label_cache: Default::default(),
             content_cache: Default::default(),
             modifier_cache: Default::default(),
+            highlighter_cache: Default::default(),
         }
     }
 }
 
+fn project_cursor_into_square(cursor_pos: &Point, sq_width: &f32, grid_dim: &u32) -> Option<(u32,u32)> {
+    let t_x: i64 = (cursor_pos.x / sq_width).floor() as i64;
+    let t_y: i64 = (cursor_pos.y / sq_width).floor() as i64;
+    match ((t_x < (*grid_dim).into()) && (t_x >= 0)) && ((t_y < (*grid_dim).into()) && (t_y >= 0)) {
+        true => Some((t_x as u32,t_y as u32)),
+        false => None,
+    }
+}
+
 impl<Message> canvas::Program<Message> for PuzzleCanvas {
+    fn update(&mut self, event: Event, bounds: Rectangle, cursor: Cursor) -> (event::Status, Option<Message>) {
+        if let Some(position) = cursor.position_in(&bounds) {
+            self.cursor_pos = position;
+            self.highlighter_cache.clear();
+        }
+        (event::Status::Captured,None)
+    }
+
     fn draw(&self, bounds: Rectangle, _cursor: Cursor,) -> Vec<canvas::Geometry> {
         let (canv_w, canv_h) = (bounds.size().width, bounds.size().height);
         let min_size = match canv_w < canv_h { 
@@ -48,6 +71,7 @@ impl<Message> canvas::Program<Message> for PuzzleCanvas {
         let label_size = 0.30 * sq_width;
 
         let mut frame_square_infos: Vec<SquareUIInfo> = Vec::with_capacity(self.dim as usize * self.dim as usize);
+        // Column-major
         for x in 0..self.dim {
             for y in 0..self.dim {
                 let p = Point {
@@ -126,7 +150,23 @@ impl<Message> canvas::Program<Message> for PuzzleCanvas {
                 }
             }
         });
+
+        let highlighter = self.highlighter_cache.draw(bounds.size(), |frame| {
+            let pc_tuple: Option<(Path,Color)> = match self.highlighted_square {
+                None => {
+                    match project_cursor_into_square(&self.cursor_pos,&sq_width, &self.dim) {
+                        Some((sx,sy)) => Some((Path::rectangle(frame_square_infos[sx as usize * self.dim as usize + sy as usize].content_top_left_corner,Size::new(content_width,content_width)),Color::from_rgba(1.0,1.0,0.0,0.3))),
+                        None => None,
+                        }
+                    },
+                Some((hx,hy)) => Some((Path::rectangle(frame_square_infos[hx as usize * self.dim as usize + hy as usize].content_top_left_corner,Size::new(content_width,content_width)),Color::from_rgba(1.0,1.0,0.0,0.5)))
+            };
+            match pc_tuple {
+                Some((p,c)) => {frame.fill(&p,c);},
+                None => {}
+            };
+        });
         
-        vec![grid,labels,content,modifiers]
+        vec![grid,labels,content,modifiers,highlighter]
     }
 }
